5e933a609a3e409a569036a8e1146721ccac0f1b
pirogrammer12 <125754914+pirogrammer12@users.noreply.github.com>
2023-11-30 19:03:44 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,9 +1,7 @@
-
-
 const socketIO = require('socket.io')
 const express = require('express');
 const app = express();
-const port = 3000;
+const port = 8080;
 const server = require('http').createServer(app);
 const io = require('socket.io')(server);
 
